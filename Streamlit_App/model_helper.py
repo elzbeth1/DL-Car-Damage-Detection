fb57e07e62ce046aa2358cebcffa84659232d03c
elzbeth1 <elzabeth0179@gmail.com>
2026-05-24 23:43:48 +0530

    path update

--- Streamlit_App/model_helper.py
+++ Streamlit_App/model_helper.py
@@ -2,9 +2,16 @@ from PIL import Image
 import torch
 from torch import nn
 from torchvision import models, transforms
+import os
 
 trained_model=None
 class_names=["Front Breakage","Front Crushed","Front Normal","Rear Breakage","Rear Crushed","Rear Normal"]
+MODEL_PATH = os.path.join(
+    os.path.dirname(__file__),
+    "..",
+    "Model",
+    "saved_model.pth"
+)
 
 #load the pretrained resnet model
 class CarClassifierResNet(nn.Module):
@@ -38,13 +45,13 @@ def predict(image_path):
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485,0.456,0.406],std=[0.229,0.224,0.225])
     ])
-    image_tensor=transform(image).unsqueeze(0) #(1,3,224,224) beccause our nn takes 32 images per batch
+    image_tensor=transform(image).unsqueeze(0) #(1,3,224,224) because our nn takes 32 images per batch
 
     global trained_model
 
     if not trained_model: #to avoid loading the model multiple times
         trained_model=CarClassifierResNet()
-        trained_model.load_state_dict(torch.load("../Model/saved_model.pth",map_location=torch.device('cpu')))  #Loading the saved model parameters to trained model variable
+        trained_model.load_state_dict(torch.load(MODEL_PATH,map_location=torch.device('cpu')))  #Loading the saved model parameters to trained model variable
         trained_model.eval()
 
     with torch.no_grad():
