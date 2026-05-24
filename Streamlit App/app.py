0e5153fb09e926ee9329eb4ce2f95f062c89c471
elzbeth1 <elzabeth0179@gmail.com>
2026-05-24 14:29:36 +0530

    Initial Commit

---
+++ Streamlit App/app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from model_helper import predict
+
+st.title("Vehicle Damage Detection")
+
+uploaded_file=st.file_uploader("Upload the Car Image",type=["jpg","png"])
+
+if uploaded_file:
+    image_path="temp_file.jpg"
+    with open(image_path,"wb") as f: # wb means write binary, f is the file handler
+        f.write(uploaded_file.getbuffer()) # uploaded image is converted to binary data and that binary data is written to a file
+        st.image(uploaded_file,caption="Uploaded File",use_container_width=True)
+        prediction=predict(image_path)
+        st.info(f"predicted Class is : {prediction}")
